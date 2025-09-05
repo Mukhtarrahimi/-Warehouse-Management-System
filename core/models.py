2c49198e5edae019984b5de3161e61fdd4cd3842
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-09-05 21:33:40 +0430

    class supplier add to models.py

--- core/models.py
+++ core/models.py
@@ -6,3 +6,10 @@ class User(AbstractUser):
         ADMIN = 'ADMIN', 'Admin'
         STAFF = 'STAFF', 'Staff'
     role = models.CharField(max_length=10, choices=Roles.choices, default=Roles.ADMIN)
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=120)
+    phone = models.CharField(max_length=50, blank=True)
+    address = models.TextField(blank=True)
+    def __str__(self):
+        return self.name
