b3156806341fcda5ddc96b8cdb55b3f0caffde66
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-09-05 21:35:46 +0430

    class product add to models.py

--- core/models.py
+++ core/models.py
@@ -20,3 +20,16 @@ class Customer(models.Model):
     address = models.TextField(blank=True)
     def __str__(self):
         return self.name
+    
+
+class Product(models.Model):
+    name = models.CharField(max_length=120)
+    code = models.CharField(max_length=60, unique=True)
+    category = models.CharField(max_length=80, blank=True)
+    purchase_price = models.DecimalField(max_digits=12, decimal_places=2, default=0)  # last purchase price
+    sale_price = models.DecimalField(max_digits=12, decimal_places=2, default=0)      # default sale price
+    stock = models.IntegerField(default=0)
+    avg_cost = models.DecimalField(max_digits=12, decimal_places=4, default=0)        # moving average cost
+    low_stock_threshold = models.IntegerField(default=5)
+    def __str__(self):
+        return f"{self.name} ({self.code})"
