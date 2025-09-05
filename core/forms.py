4fdfda9ad83ad1e31d64cc8bd469bb14c10ef1c8
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-09-05 21:40:22 +0430

    class login add to forms.py

--- core/forms.py
+++ core/forms.py
@@ -1,3 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from .models import Product, Supplier, Customer, StockIn, StockOut
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':'input'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'input'}))
