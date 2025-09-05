e9ceded4f273abc990fd093729121c00bbbf83e6
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-09-05 21:37:06 +0430

    class stockin add to models.py

--- core/models.py
+++ core/models.py
@@ -33,3 +33,29 @@ class Product(models.Model):
     low_stock_threshold = models.IntegerField(default=5)
     def __str__(self):
         return f"{self.name} ({self.code})"
+    
+class StockIn(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='stockins')
+    supplier = models.ForeignKey(Supplier, on_delete=models.SET_NULL, null=True, blank=True)
+    quantity = models.PositiveIntegerField()
+    unit_cost = models.DecimalField(max_digits=12, decimal_places=2)
+    date = models.DateField(auto_now_add=True)
+    note = models.CharField(max_length=200, blank=True)
+
+    def save(self, *args, **kwargs):
+        with transaction.atomic():
+            creating = self._state.adding
+            super().save(*args, **kwargs)
+            if creating:
+                # update product stock and moving average cost
+                p = self.product
+                old_stock = p.stock
+                old_avg = p.avg_cost
+                new_qty = self.quantity
+                new_cost = self.unit_cost
+                total_cost = old_stock * old_avg + new_qty * new_cost
+                new_stock = old_stock + new_qty
+                p.stock = new_stock
+                p.avg_cost = (total_cost / new_stock) if new_stock > 0 else 0
+                p.purchase_price = new_cost
+                p.save()
