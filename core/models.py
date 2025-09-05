46851e6b4046ab9ec7548d82a4c413d302bc12eb
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-09-05 21:32:35 +0430

    user class add to models.py

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
-from django.db import models
+from django.db import models, transaction
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    class Roles(models.TextChoices):
+        ADMIN = 'ADMIN', 'Admin'
+        STAFF = 'STAFF', 'Staff'
+    role = models.CharField(max_length=10, choices=Roles.choices, default=Roles.ADMIN)
