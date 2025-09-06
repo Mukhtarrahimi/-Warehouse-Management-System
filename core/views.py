df3ba7fdacceebfa68374cd75003a04989ba513c
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-09-06 21:49:07 +0430

    libraray add to views.py

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 def login_view(request):
     if request.user.is_authenticated:
