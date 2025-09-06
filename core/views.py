d485100470aed5babba2fa552ef54da9ac0f5859
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-09-06 21:46:57 +0430

    login -> views.py

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('dashboard')
+    form = LoginForm(request, data=request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        user = form.get_user()
+        login(request, user)
+        return redirect('dashboard')
+    return render(request, 'auth/login.html', {'form': form})
