b6737776e3585bc976c73cc76a2b3883703acc4d
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-09-07 22:02:44 +0430

    customer_create -> views.py

--- core/views.py
+++ core/views.py
@@ -115,3 +115,11 @@ def supplier_delete(request, pk):
 @login_required
 def customer_list(request):
     return render(request, 'customers/list.html', {'customers': Customer.objects.all()})
+
+@login_required
+@admin_required
+def customer_create(request):
+    form = CustomerForm(request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        form.save(); return redirect('customer_list')
+    return render(request, 'customers/form.html', {'form': form, 'title':'Create Customer'})
