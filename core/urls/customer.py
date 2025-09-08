e42f005d61e83030bec62fd1e260b1b2d7c0cfc5
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-09-08 22:16:43 +0430

    customer.py url update

--- core/urls/customer.py
+++ core/urls/customer.py
@@ -1,13 +1,10 @@
-@login_required
-def profit_report(request):
-    start = request.GET.get('start')
-    end = request.GET.get('end')
-    qs = StockOut.objects.all()
-    if start:
-        qs = qs.filter(date__gte=start)
-    if end:
-        qs = qs.filter(date__lte=end)
-    from django.db.models import DecimalField
-    total_revenue = qs.aggregate(val=Sum(F('unit_price')*F('quantity'), output_field=DecimalField(max_digits=12, decimal_places=2)))['val'] or 0
-    total_profit = qs.aggregate(val=Sum((F('unit_price')-F('cost_basis'))*F('quantity'), output_field=DecimalField(max_digits=12, decimal_places=2)))['val'] or 0
-    return render(request, 'reports/profit.html', {'items': qs.order_by('-id'), 'total_revenue': total_revenue, 'total_profit': total_profit, 'start': start, 'end': end})
+
+from django.urls import path
+from core import views
+
+urlpatterns = [
+    path('', views.customer_list, name='customer_list'),
+    path('create/', views.customer_create, name='customer_create'),
+    path('<int:pk>/edit/', views.customer_edit, name='customer_edit'),
+    path('<int:pk>/delete/', views.customer_delete, name='customer_delete'),
+]
